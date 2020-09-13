6b344b84c4a3f2371d5804593ed08838eac90e40
Ayush Singh <singhayush1403@gmail.com>
2020-09-13 21:20:23 +0530

    user check added

--- cropProject/views.py
+++ cropProject/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import Context, loader
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+
 def index(response):
     return HttpResponse("<h1>tech with tim!</h1>")
 
 def v1(response):
     return HttpResponse("<h1>View 1!</h1>")
-
+@login_required(login_url='/accounts/login')
 def profile(response):
-    print(response)
-    template = loader.get_template("registration/profile.html")
-    return HttpResponse(template.render())
+    print(response.user.is_authenticated)
+    context = {
+      "user":response.user
+      }
+    #template = loader.get_template("registration/profile.html")
+    return render(response,"registration/profile.html",context)
